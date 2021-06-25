8b6eb142097ac5d5cfbc8633cf0fc4cfd1fe0350
chenzuo <348826240@qq.com>
2021-06-25 18:10:56 +0800

    <A> 增加 pr转换rem

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,14 @@ module.exports = {
     },
     css: {
       loaderOptions: {
+        postcss: {
+          plugins: [
+            require('postcss-pxtorem')({ // 把px单位换算成rem单位
+              rootValue: 14, // 换算的基数：设计稿元素尺寸/rootValue，比如元素宽28px,最终页面会换算成 2rem
+              propList: ['*']
+            })
+          ]
+        },
         sass: {
           prependData: `
             @import "@/style/variables.scss";
